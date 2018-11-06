b98e272323098b4d50db28676cb714eec7ef674b
nancylicona <nancylicona@utexas.edu>
2018-11-06 17:21:45 -0600

    pushitpushit...

--- player.cpp
+++ player.cpp
@@ -2,3 +2,14 @@
 #include "player.h"
 
 using namespace std;
+
+Player::Player() {
+    myName="";
+    myHand.resize(0);
+    myBook.resize(0);
+}
+
+void Player::addCard(Card c)  
+{
+    myHand.push_back(c);        //adds a card to the hand
+}
