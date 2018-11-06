c78fd7230bb4f530d0d9eaa00690bfbc72f6f7c4
arevelo23 <asrevelo23@utexas.edu>
2018-11-06 17:23:36 -0600

    To the limit limit!!!

--- player.cpp
+++ player.cpp
@@ -9,7 +9,16 @@ Player::Player() {
     myBook.resize(0);
 }
 
-void Player::addCard(Card c)  
+void Player::addCard(Card c)
 {
-    myHand.push_back(c);        //adds a card to the hand
+    myHand.push_back(c);                            //adds a card to the hand
 }
+
+
+void Player::bookCards(Card c1, Card c2){
+    if(c1==c2){                                     // if cards are the same rank and spades
+        myBook.push_back(c1);                       //Add card1 to the book
+        myBook.push_back(c2);                       //Add card2 to the book
+    }
+}
+
