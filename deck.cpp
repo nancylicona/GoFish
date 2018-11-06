979c566066e33e07364fb67f36dc33d5a3397a9b
nancylicona <nancylicona@utexas.edu>
2018-11-06 16:56:28 -0600

    deck finished?

--- deck.cpp
+++ deck.cpp
@@ -5,42 +5,40 @@
 using namespace std;
 
 Deck::Deck()           // pristine, sorted deck
-{   int j=1;
-    for(int suit = Card::spades; suit<=Card::clubs; suit++){        //casting the numbers for suits
-        Card::Suit  suit2 = static_cast <Card::Suit> (suit); // Casting???? not understanding
-        for(int r=0; r<13;r++){             //
-            myCards[j] = Card(r,(Card::Suit)suit);          //
-            j++;                //
+{   int j=0;		//myCards array index
+    for(int suit = Card::spades; suit<=Card::clubs; suit++){        //looping through all the suits
+        Card::Suit  suit2 = static_cast <Card::Suit> (suit); 	   // Casting suit to change datatype int to suit
+        for(int r=1;r<14;r++){            	 //looping through all the ranks
+            myCards[j] = Card(r,suit2);          //storing card in array
+            j++;               			 //incrementing array index
         }
     }
 }
 
-void Deck::shuffle()   // shuffle the deck, all 52 cards present
+void Deck::shuffle()   // shuffle the deck
 {
-    for(int i=0; i < SIZE; i++){             //shuffles 52 cards
-        int card1 = (rand() % SIZE) + 1;       //getting random rank
-        int card2 = (rand() % SIZE) + 1;        //getting random suit
-        if (card1!=card2){
-            int temp = card1;
-            myCards[card1]=myCards[card2];
-            myCards[card2]=myCards[temp];
+    for(int i=0; i < size(); i++){             //loops through all the remaining cards in the deck
+        int card1 = (rand() % size()) + 1;     //getting random for myCard index
+        int card2 = (rand() % size()) + 1;     //getting second random for myCard index
+        if (card1!=card2){			//if they are not the same random numbers
+            int temp = card1;			//set temporary
+            myCards[card1]=myCards[card2];	//swap the...
+            myCards[card2]=myCards[temp];	//...cards
         }
     }
 }
 
 Card Deck::dealCard()   // get a card, after 52 are dealt, fail
 {
-    if ((size() > 0)&&(myIndex < 51)) {
-        Card dealbaby = myCards[myIndex];
-        current--;
-        currentsize->resize(current);
-        return dealbaby;
+    Card dealbaby=myCards[myIndex];		//dealbaby hold the exact card with index [myIndex]
+    if (size() > 0) {				//when deck is not empty
+        myIndex++;				//increment the index to get the next card
+        return dealbaby;			//deal the new card 
     }
-    else return              //fail
+    else return dealbaby;           		//fixing condition when playing
 }
 
 int  Deck::size() const // # cards left in the deck
 {
-
-    return currentsize->size();
+    return (SIZE-myIndex+1);			//Return the number of cards left in the deck 
 }
