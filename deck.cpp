92606c06635926c84cbddd069c66ff5c64a9dd8e
arevelo23 <asrevelo23@utexas.edu>
2018-11-06 22:04:01 -0600

    Parenthesis

--- deck.cpp
+++ deck.cpp
@@ -20,8 +20,8 @@ void Deck::shuffle()   // shuffle the remaining deck
 {
     for(int i=0; i < size(); i++){             //loops through all the remaining cards in the deck
         int temp=SIZE - myIndex -1;             //temp to shuffle the right random
-        int card1 = (rand() % temp + myIndex;     //getting random for myCard index
-        int card2 = (rand() % temp + myIndex;     //getting second random for myCard index
+        int card1 = (rand() % temp + myIndex);     //getting random for myCard index
+        int card2 = (rand() % temp + myIndex);     //getting second random for myCard index
         if (card1!=card2 && size()>1){			//if they are not the same random numbers
             int temp = card1;			//set temporary
             myCards[card1]=myCards[card2];	//swap the...
@@ -36,12 +36,12 @@ Card Deck::dealCard()   // get a card, after 52 are dealt, fail
     Card dealbaby=myCards[myIndex];		//dealbaby hold the exact card with index [myIndex]
     if (size() > 0) {				//when deck is not empty
         myIndex++;				//increment the index to get the next card
-        return dealbaby;			//deal the new card 
+        return dealbaby;			//deal the new card
     }
     else return dealbaby;           		//fixing condition when playing
 }
 
 int  Deck::size() const // # cards left in the deck
 {
-    return (SIZE-myIndex+1);			//Return the number of cards left in the deck 
+    return (SIZE-myIndex+1);			//Return the number of cards left in the deck
 }
